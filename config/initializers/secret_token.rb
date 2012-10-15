e57290254062798045a51c87bddd79a9c8eedd2d
tannv85 <tannv85.@gmail.com>
2012-10-15 10:15:30 +0700

    first initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Resourcemanager::Application.config.secret_token = 'a98af3f1887ac941768766e43e0984fee4b28a5f48a39d8aacf603ef7e8c49436bb6f248a1c3a4b5b77336ba1040da70d3fcf7361a5515f2e97f0be7aba5bb4f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Resourcemanager::Application.config.session_store :cookie_store, key: '_resourcemanager_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Resourcemanager::Application.config.session_store :active_record_store
